bae751983289834bf5194f636a3cd9503d24de1b
evan <esims89@gmail.com>
2013-06-25 00:31:24 -0700

    Working graphs from data from db

--- client/views/graphs/graphs.js
+++ client/views/graphs/graphs.js
@@ -1,4 +1,20 @@
+// numerator, denominator, time
+var prepStats = function(ary) {
+  var n = ary[0];
+  var d = ary[1];
+  return {y: n/d, x: ary[2]};
+}
+
+// returns an object with fields of prepared data
+var prepareData = function(stats) {
+  var res = Object;
+  res.hps = _.map(_.zip(stats.helpfuls, stats.students, stats.times), prepStats);
+  res.cpr = _.map(_.zip(stats.comments, stats.responses, stats.times), prepStats);
+  return res;
+};
+
 var makeGraph = function () {
+  var data = prepareData(Classes.findOne().statsDigest);
   var graph = new Rickshaw.Graph( {
   	element: document.querySelector("#graph"),
   	width: 260,
@@ -7,13 +23,15 @@ var makeGraph = function () {
   	stroke: true,
   	series: [ {
       name: "Avg Helpfuls per student",
-  		data: [ { x: 0, y: 40 }, { x: 1, y: 49 }, { x: 2, y: 38 }, { x: 3, y: 20 }, { x: 4, y: 16 } ],
-  		color: 'rgba(192,210,225,0.5)',
+      data: data.hps,
+      // data: [ { x: 0, y: 40 }, { x: 1, y: 49 }, { x: 2, y: 38 }, { x: 3, y: 20 }, { x: 4, y: 16 } ],
+  		color: 'rgba(192,210,225,0.7)',
   		stroke: 'rgba(0,0,0,0.15)'
   	}, {
       name: "Avg. Comments per response",
-  		data: [ { x: 0, y: 22 }, { x: 1, y: 25 }, { x: 2, y: 38 }, { x: 3, y: 44 }, { x: 4, y: 46 } ],
-  		color: 'rgba(230,240,226,0.5)',
+      data: data.cpr,
+      // data: [ { x: 0, y: 22 }, { x: 1, y: 25 }, { x: 2, y: 38 }, { x: 3, y: 44 }, { x: 4, y: 46 } ],
+  		color: 'rgba(230,240,226,0.7)',
   		stroke: 'rgba(0,0,0,0.15)'
   	} ]
   } );
@@ -45,4 +63,3 @@ var makeGraph = function () {
 };
 
 Meteor.startup(makeGraph);
-color="#c0d2e1"
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,6 @@
 if (Classes.find().count() === 0) {
   
   Classes.insert({
-    'Science': {
       name         : 'Science',
       code         : 'SC710',
       subject      : 'Science',
@@ -10,11 +9,12 @@ if (Classes.find().count() === 0) {
       _teacherName : 'Tom Halbert',
 
       statsDigest: {
-        time: ["20130101", "20130201", "20130301", "20130401", "20130501", "20130601", "20130701"],
+        // times: ["20130101", "20130201", "20130301", "20130401", "20130501", "20130601", "20130701"],
+        times: [1,2,3,4,5,6,7],
         students: [21, 22, 23, 24, 25, 26, 27],
         helpfuls: [5, 10, 15, 20, 25, 30, 35],
-        comments: [2, 4, 8, 16, 32, 64, 128]
-      }
+        comments: [2, 4, 8, 16, 32, 64, 128],
+        responses: [10, 20, 30, 40, 50, 60, 70]
     }
   })
 }
