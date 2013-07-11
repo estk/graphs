6faff1ac9e9cff16fd3f81bda48c2dba950648e5
Evan Simmons <esims89@gmail.com>
2013-07-11 10:52:06 -0700

    Removed console logging and client side live data generation.

--- client/views/graphs/class.js
+++ client/views/graphs/class.js
@@ -12,14 +12,12 @@ Template.graphsClass.rendered = function() {
       var cls = Classes.findOne();
       var element = document.querySelector("#graph");
 
-      console.log('cls is:' + cls);
       if (cls) {
         cid = cls._id;
         var statary = ClassStatistics
           .find({classId: cls._id}, {sort: {date: -1}, limit:11})
           .fetch();
         stats = _.sortBy(statary, function(o){return o.date});
-        console.log('stats is:' + stats);
 
         if (stats) {
           MyGraph.init(element, stats);
@@ -43,21 +41,5 @@ var reRenderGraph = function() {
 // Event Listeners
 Meteor.startup( function () {
   $(window).on('resize', reRenderGraph);
-
-  // setInterval( function() {
-  //   hc = cc = rc = sc = 1;
-  //   hc+=Math.round( Math.random()*5 )+1;
-  //   cc+=Math.round( Math.random()*5 )+1;
-  //   rc+=Math.round( Math.random()*5 )+1;
-  //   sc+=Math.round( Math.random()*5 )+1;
-  //   ClassStatistics.insert({
-  //     classId: cid,
-  //     date: new Date().getTime(),
-  //     helpfulCount: hc,
-  //     commentCount: cc,
-  //     responseCount: rc,
-  //     studentCount: sc
-  //   });
-  // }, 5000 );
 });
 
