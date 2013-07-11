664a96b372ab9db65574847ede0057ce27191fe7
Evan Simmons <esims89@gmail.com>
2013-07-11 11:05:13 -0700

    Tweaked live data.

--- server/live.js
+++ server/live.js
@@ -6,15 +6,15 @@ Meteor.startup( function () {
       {date: {$gte: startToday, $lt: endToday}},
       {$inc: {studentCount: 1, commentCount: 5}}
     );
-  }, 5000 )
-  Meteor.setInterval( function() {
-    var tomorrow = moment().add("days", 1).toDate().getTime();
-    ClassStatistics.insert({
-      classId: cid,
-      date: tomorrow,
-      helpfulCount: 20,
-      commentCount: 20,
-      responseCount: 20,
-      studentCount: 20
-    });  }, 10000 )
+  }, 5000 );
+  // Meteor.setTimeout( function() {
+  //   var tomorrow = moment().add("days", 1).toDate().getTime();
+  //   ClassStatistics.insert({
+  //     classId: cid,
+  //     date: tomorrow,
+  //     helpfulCount: 20,
+  //     commentCount: 20,
+  //     responseCount: 20,
+  //     studentCount: 20
+  //   });  }, 10000 );
 } );
