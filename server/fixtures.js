67f4ba490a4fc04c2f524f6fd7db71712f569dbb
Zoltan Olah <zol@me.com>
2013-07-18 16:53:25 -0700

    Resetting fixtures on app launch.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,30 +1,31 @@
-if (Classes.find().count() === 0 || ClassStatistics.find().count() === 0) {
-  // Create a sample class.
-  var cid = Classes.insert({
-    name: 'Science',
-    code: 'SC710',
-    subject: 'Science',
-    year: '7.1',
-    status: 'open',
-    _teacherName: 'Tom Halbert',
+Classes.remove({}, {multi: true});
+ClassStatistics.remove({}, {multi: true});
+
+// Create a sample class.
+var cid = Classes.insert({
+  name: 'Science',
+  code: 'SC710',
+  subject: 'Science',
+  year: '7.1',
+  status: 'open',
+  _teacherName: 'Tom Halbert',
+});
+// Prepare and insert sample statistics for the class
+var hc = 1, cc = 1, rc = 1, sc = 1;
+var date = moment().subtract("days", 19);
+_.times(20, function (n) {
+  hc += Math.round(Math.random() * 5 );
+  cc += Math.round(Math.random() * 5 );
+  rc += Math.round(Math.random() * 5 );
+  sc += Math.round(Math.random() * 5 );
+  
+  ClassStatistics.insert({
+    classId: cid,
+    date: date.toDate().getTime(),
+    helpfulCount: hc,
+    commentCount: cc,
+    responseCount: rc,
+    studentCount: sc
   });
-  // Prepare and insert sample statistics for the class
-  var hc = 1, cc = 1, rc = 1, sc = 1;
-  var date = moment().subtract("days", 19);
-  _.times(20, function (n) {
-    hc += Math.round(Math.random() * 5 );
-    cc += Math.round(Math.random() * 5 );
-    rc += Math.round(Math.random() * 5 );
-    sc += Math.round(Math.random() * 5 );
-    
-    ClassStatistics.insert({
-      classId: cid,
-      date: date.toDate().getTime(),
-      helpfulCount: hc,
-      commentCount: cc,
-      responseCount: rc,
-      studentCount: sc
-    });
-    date = date.add("days", 1);
-  });
-}
+  date = date.add("days", 1);
+});
